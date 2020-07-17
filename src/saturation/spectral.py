74ff2fabfe6e864a383becb504f66e644210125d
Ingmar Rieger <git@irieger.net>
2020-07-17 22:42:46 +0200

    Finishing replacement of old code and running a successful test on some image from the dataset for verification.

--- src/saturation/spectral.py
+++ src/saturation/spectral.py
@@ -6,9 +6,20 @@ from .meng import Meng
 
 
 ksm_folder  = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../datasets/ksm_dataset_128'))
-ksm_calc    = KSM(ksm_folder)
+ksm_calc    = KSM.load(ksm_folder)
 meng_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../datasets/meng_dataset_128'))
 meng_calc   = Meng.load(meng_folder)
+
+
+def lumaPreserve(org, saturated, luma_preserve = 0.0):
+    if luma_preserve < 0.0 or luma_preserve > 1.0:
+        raise Exception('Invalid luma_preserve value')
+    new_luma = saturated[...,1].copy()
+    idx = new_luma < 0.00000001
+    new_luma[idx] = 1.0
+    mult      = np.divide(org[...,1], new_luma) * luma_preserve + np.ones(new_luma.shape) * (1 - luma_preserve)
+    mult[idx] = 1.0
+    return np.multiply(saturated, mult[...,None])
 
 
 def ksmSaturation(xyz, saturation, luma_preserve = 0.0,
@@ -16,12 +27,8 @@ def ksmSaturation(xyz, saturation, luma_preserve = 0.0,
     """
     Saturate in KSM model
     """
-    eps = 0.00000000001
-    # Todo White point conversion if not 0.333,0.333
-    # ksm_calc   = KSM(load_folder=ksm_folder, uv_grid=True, auto_fill=True)
-    data = xyz.copy()
-    data[data<eps] = eps
-    return ksm_calc.evalImageSaturation(data, saturation, luma_preserve)
+    res = ksm_calc.processImage(saturation, xyz)
+    return lumaPreserve(xyz, res, luma_preserve)
 
 
 def mengSaturation(xyz, saturation, luma_preserve = 0.0,
@@ -29,19 +36,5 @@ def mengSaturation(xyz, saturation, luma_preserve = 0.0,
     """
     Saturate in Meng2015 model
     """
-    eps = 0.00000000001
-    # Todo White point conversion if not 0.333,0.333
-    data = xyz.copy()
-    data[data<eps] = eps
-    while len(data.shape) < 3:
-        data = data[None,...]
-    # Todo line by line to prevent memory problems
-    res_spec = np.power(meng_calc.interpolateImage(data, as_xyz=False), saturation)
-    res_xyz  = np.zeros(data.shape)
-    for y in range(res_xyz.shape[0]):
-        for x in range(res_xyz.shape[1]):
-            res_xyz[y, x, :] = meng_calc.evalXYZ(res_spec[y,x,:])
-    # res_xyz[res_xyz<eps] = eps
-    
-    y_mult = data[...,1] / res_xyz[...,1]
-    return (1 - luma_preserve) * res_xyz + luma_preserve * (res_xyz * np.repeat(y_mult[...,None], 3, axis=-1))
+    res = meng_calc.processImage(saturation, xyz)
+    return lumaPreserve(xyz, res, luma_preserve)
